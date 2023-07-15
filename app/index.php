0d49c39bc6298182e971fc0d9d9fc2a39c9100a8
atsteaqth <atsteaqth@mail.ru>
2023-07-15 19:59:38 +0300

    feeedback

--- app/index.php
+++ app/index.php
@@ -276,6 +276,341 @@ $APPLICATION->SetTitle("Главная");
 );?>
 
 
+<!-- Start Timeline Work Section -->
+<section id="timelineWork" class="timelineWork pddt100">
+    <div  class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2 class="mt-minus-10 wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1s">Куда сходить?</h2>
+                    <h4 class="wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1s">на отдыхе</h4>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="main-timeline">
+
+
+                    <?php $APPLICATION->IncludeComponent(
+                        "bitrix:news.list",
+                        "place",
+                        array(
+                            "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                            "ADD_SECTIONS_CHAIN" => "N",
+                            "AJAX_MODE" => "N",
+                            "AJAX_OPTION_ADDITIONAL" => "",
+                            "AJAX_OPTION_HISTORY" => "N",
+                            "AJAX_OPTION_JUMP" => "N",
+                            "AJAX_OPTION_STYLE" => "Y",
+                            "CACHE_FILTER" => "N",
+                            "CACHE_GROUPS" => "Y",
+                            "CACHE_TIME" => "36000000",
+                            "CACHE_TYPE" => "A",
+                            "CHECK_DATES" => "Y",
+                            "DETAIL_URL" => "",
+                            "DISPLAY_BOTTOM_PAGER" => "N",
+                            "DISPLAY_DATE" => "Y",
+                            "DISPLAY_NAME" => "Y",
+                            "DISPLAY_PICTURE" => "Y",
+                            "DISPLAY_PREVIEW_TEXT" => "Y",
+                            "DISPLAY_TOP_PAGER" => "N",
+                            "FIELD_CODE" => array(
+                                0 => "ID",
+                                1 => "CODE",
+                                2 => "XML_ID",
+                                3 => "NAME",
+                                4 => "TAGS",
+                                5 => "SORT",
+                                6 => "PREVIEW_TEXT",
+                                7 => "PREVIEW_PICTURE",
+                                8 => "DETAIL_TEXT",
+                                9 => "DETAIL_PICTURE",
+                                10 => "DATE_ACTIVE_FROM",
+                                11 => "ACTIVE_FROM",
+                                12 => "DATE_ACTIVE_TO",
+                                13 => "ACTIVE_TO",
+                                14 => "SHOW_COUNTER",
+                                15 => "SHOW_COUNTER_START",
+                                16 => "IBLOCK_TYPE_ID",
+                                17 => "IBLOCK_ID",
+                                18 => "IBLOCK_CODE",
+                                19 => "IBLOCK_NAME",
+                                20 => "IBLOCK_EXTERNAL_ID",
+                                21 => "DATE_CREATE",
+                                22 => "CREATED_BY",
+                                23 => "CREATED_USER_NAME",
+                                24 => "TIMESTAMP_X",
+                                25 => "MODIFIED_BY",
+                                26 => "USER_NAME",
+                                27 => "",
+                            ),
+                            "FILTER_NAME" => '',
+                            "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                            "IBLOCK_ID" => "3",
+                            "IBLOCK_TYPE" => "content",
+                            "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                            "INCLUDE_SUBSECTIONS" => "N",
+                            "MESSAGE_404" => "",
+                            "NEWS_COUNT" => "500",
+                            "PAGER_BASE_LINK_ENABLE" => "N",
+                            "PAGER_DESC_NUMBERING" => "N",
+                            "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                            "PAGER_SHOW_ALL" => "N",
+                            "PAGER_SHOW_ALWAYS" => "N",
+                            "PAGER_TEMPLATE" => "news",
+                            "PAGER_TITLE" => "Новости",
+                            "PARENT_SECTION" => "",
+                            "PARENT_SECTION_CODE" => "",
+                            "PREVIEW_TRUNCATE_LEN" => "",
+                            "PROPERTY_CODE" => array(
+                                0 => "",
+                                1 => "",
+                            ),
+                            "SET_BROWSER_TITLE" => "N",
+                            "SET_LAST_MODIFIED" => "N",
+                            "SET_META_DESCRIPTION" => "N",
+                            "SET_META_KEYWORDS" => "N",
+                            "SET_STATUS_404" => "Y",
+                            "SET_TITLE" => "N",
+                            "SHOW_404" => "N",
+                            "SORT_BY1" => "ACTIVE_FROM",
+                            "SORT_BY2" => "SORT",
+                            "SORT_ORDER1" => "DESC",
+                            "SORT_ORDER2" => "ASC",
+                            "STRICT_SECTION_CHECK" => "N",
+                            "COMPONENT_TEMPLATE" => "place"
+                        ),
+                        false
+                    );
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Timeline Work Section -->
+
+
+<section id="review-app" class="timelineWork pddt100">
+    <div  class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title">
+                    <h2 class="mt-minus-10 wow fadeInUp" data-wow-delay=".2s" data-wow-duration="1s">Отзывы</h2>
+                    <h4 class="wow fadeInUp" data-wow-delay=".4s" data-wow-duration="1s">от наших клиентов</h4>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="post-comments">
+                    <div class="post-comments-content">
+                        <div class="comments-wrapper">
+                            <ul class="comments-lists">
+
+                                <? $APPLICATION->IncludeComponent(
+                                    "bitrix:news.list",
+                                    "comments",
+                                    array(
+                                        "ACTIVE_DATE_FORMAT" => "d.m.Y",
+                                        "ADD_SECTIONS_CHAIN" => "N",
+                                        "AJAX_MODE" => "N",
+                                        "AJAX_OPTION_ADDITIONAL" => "",
+                                        "AJAX_OPTION_HISTORY" => "N",
+                                        "AJAX_OPTION_JUMP" => "N",
+                                        "AJAX_OPTION_STYLE" => "Y",
+                                        "CACHE_FILTER" => "N",
+                                        "CACHE_GROUPS" => "Y",
+                                        "CACHE_TIME" => "36000000",
+                                        "CACHE_TYPE" => "A",
+                                        "CHECK_DATES" => "Y",
+                                        "DETAIL_URL" => "",
+                                        "DISPLAY_BOTTOM_PAGER" => "N",
+                                        "DISPLAY_DATE" => "Y",
+                                        "DISPLAY_NAME" => "Y",
+                                        "DISPLAY_PICTURE" => "Y",
+                                        "DISPLAY_PREVIEW_TEXT" => "Y",
+                                        "DISPLAY_TOP_PAGER" => "N",
+                                        "FIELD_CODE" => array(
+                                            0 => "ID",
+                                            1 => "CODE",
+                                            2 => "XML_ID",
+                                            3 => "NAME",
+                                            4 => "TAGS",
+                                            5 => "SORT",
+                                            6 => "PREVIEW_TEXT",
+                                            7 => "PREVIEW_PICTURE",
+                                            8 => "DETAIL_TEXT",
+                                            9 => "DETAIL_PICTURE",
+                                            10 => "DATE_ACTIVE_FROM",
+                                            11 => "ACTIVE_FROM",
+                                            12 => "DATE_ACTIVE_TO",
+                                            13 => "ACTIVE_TO",
+                                            14 => "SHOW_COUNTER",
+                                            15 => "SHOW_COUNTER_START",
+                                            16 => "IBLOCK_TYPE_ID",
+                                            17 => "IBLOCK_ID",
+                                            18 => "IBLOCK_CODE",
+                                            19 => "IBLOCK_NAME",
+                                            20 => "IBLOCK_EXTERNAL_ID",
+                                            21 => "DATE_CREATE",
+                                            22 => "CREATED_BY",
+                                            23 => "CREATED_USER_NAME",
+                                            24 => "TIMESTAMP_X",
+                                            25 => "MODIFIED_BY",
+                                            26 => "USER_NAME",
+                                            27 => "",
+                                        ),
+                                        "FILTER_NAME" => '',
+                                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                                        "IBLOCK_ID" => "4",
+                                        "IBLOCK_TYPE" => "content",
+                                        "INCLUDE_IBLOCK_INTO_CHAIN" => "N",
+                                        "INCLUDE_SUBSECTIONS" => "N",
+                                        "MESSAGE_404" => "",
+                                        "NEWS_COUNT" => "500",
+                                        "PAGER_BASE_LINK_ENABLE" => "N",
+                                        "PAGER_DESC_NUMBERING" => "N",
+                                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                                        "PAGER_SHOW_ALL" => "N",
+                                        "PAGER_SHOW_ALWAYS" => "N",
+                                        "PAGER_TEMPLATE" => "news",
+                                        "PAGER_TITLE" => "Отзывы",
+                                        "PARENT_SECTION" => "",
+                                        "PARENT_SECTION_CODE" => "",
+                                        "PREVIEW_TRUNCATE_LEN" => "",
+                                        "PROPERTY_CODE" => array(
+                                            0 => "ATT_SERVICE",
+                                            1 => "ATT_PLACING",
+                                            2 => "ATT_CLEAR",
+                                            3 => "ATT_PRICE",
+                                            4 => "ATT_EMAIL",
+                                        ),
+                                        "SET_BROWSER_TITLE" => "N",
+                                        "SET_LAST_MODIFIED" => "N",
+                                        "SET_META_DESCRIPTION" => "N",
+                                        "SET_META_KEYWORDS" => "N",
+                                        "SET_STATUS_404" => "Y",
+                                        "SET_TITLE" => "N",
+                                        "SHOW_404" => "N",
+                                        "SORT_BY1" => "ACTIVE_FROM",
+                                        "SORT_BY2" => "SORT",
+                                        "SORT_ORDER1" => "DESC",
+                                        "SORT_ORDER2" => "ASC",
+                                        "STRICT_SECTION_CHECK" => "N",
+                                        "COMPONENT_TEMPLATE" => "place"
+                                    ),
+                                    false
+                                );
+
+                                ?>
+
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6" >
+
+                <div class="reply-comments wow fadeInUp" data-wow-duration="1s">
+                    <div class="sidebar-title">
+                        <h4>Оставьте свой отзыв</h4>
+                    </div>
+                    <div class="reply-comment-content box-sidebar">
+                        <form id="sendReply" action="/ajax/sendReply.php" method="post">
+                            <input type="hidden" name="service">
+                            <input type="hidden" name="placing">
+                            <input type="hidden" name="clear">
+                            <input type="hidden" name="price">
+                            <div class="row">
+                                <!-- Start Single Input -->
+                                <div class="col-12">
+                                    <div class="stars">
+                                        <div class="stars__name">Сервис</div>
+                                        <div class="stars__wrapper">
+                                            <div class="fa fa-star stars__item" data-value="1" data-name="service"></div>
+                                            <div class="fa fa-star stars__item" data-value="2" data-name="service"></div>
+                                            <div class="fa fa-star stars__item" data-value="3" data-name="service"></div>
+                                            <div class="fa fa-star stars__item" data-value="4" data-name="service"></div>
+                                            <div class="fa fa-star stars__item" data-value="5" data-name="service"></div>
+                                        </div>
+                                    </div>
+                                    <div class="stars">
+                                        <div class="stars__name">Размещение</div>
+                                        <div class="stars__wrapper">
+                                            <div class="fa fa-star stars__item" data-value="1" data-name="placing"></div>
+                                            <div class="fa fa-star stars__item" data-value="2" data-name="placing"></div>
+                                            <div class="fa fa-star stars__item" data-value="3" data-name="placing"></div>
+                                            <div class="fa fa-star stars__item" data-value="4" data-name="placing"></div>
+                                            <div class="fa fa-star stars__item" data-value="5" data-name="placing"></div>
+                                        </div>
+                                    </div>
+                                    <div class="stars">
+                                        <div class="stars__name">Чистота</div>
+                                        <div class="stars__wrapper">
+                                            <div class="fa fa-star stars__item" data-value="1" data-name="clear"></div>
+                                            <div class="fa fa-star stars__item" data-value="2" data-name="clear"></div>
+                                            <div class="fa fa-star stars__item" data-value="3" data-name="clear"></div>
+                                            <div class="fa fa-star stars__item" data-value="4" data-name="clear"></div>
+                                            <div class="fa fa-star stars__item" data-value="5" data-name="clear"></div>
+                                        </div>
+                                    </div>
+                                    <div class="stars">
+                                        <div class="stars__name">Соотношение цены и качества</div>
+                                        <div class="stars__wrapper">
+                                            <div class="fa fa-star stars__item" data-value="1" data-name="price"></div>
+                                            <div class="fa fa-star stars__item" data-value="2" data-name="price"></div>
+                                            <div class="fa fa-star stars__item" data-value="3" data-name="price"></div>
+                                            <div class="fa fa-star stars__item" data-value="4" data-name="price"></div>
+                                            <div class="fa fa-star stars__item" data-value="5" data-name="price"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="reply-input">
+                                        <input type="text" id="name-reply" name="name" placeholder="Имя" required="">
+                                    </div>
+                                </div>
+                                <!-- End Single Input -->
+                                <!-- Start Single Input -->
+                                <div class="col-md-6 col-sm-6">
+                                    <div class="reply-input">
+                                        <input type="email" id="email-reply" name="email" placeholder="E-Mail" required="">
+                                    </div>
+                                </div>
+                                <!-- End Single Input -->
+                                <!-- Start Single Textarea -->
+                                <div class="col-md-12">
+                                    <div class="reply-textarea">
+                                        <textarea id="msg-reply" name="message" placeholder="Комментарий" required=""></textarea>
+                                    </div>
+                                </div>
+                                <!-- End Single Textarea -->
+                                <!-- Start Single Button -->
+                                <div class="col-md-12">
+                                    <div class="button-MSG">
+                                        <button type="submit" class="bttn-vanda-gradient">
+                                            <i class="fa fa-comment"></i>
+                                            <span>Отправить</span>
+                                        </button>
+                                    </div>
+                                </div>
+                                <!-- End Single Button -->
+                            </div>
+                        </form>
+                        <div id="sendResult"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+
 <?php
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
 ?>
