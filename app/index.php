07f15891ac3e8d43dd3b2e84fce4d17011d46453
atsteaqth <atsteaqth@mail.ru>
2023-07-15 16:06:56 +0300

    welcom ekspediciyadom

--- app/index.php
+++ app/index.php
@@ -1,9 +1,37 @@
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/header.php');
 $APPLICATION->SetTitle('Главная');
-?> 
+?>
 
+    <!-- Start Container -->
+    <div class="container">
+        <div class="row">
+            <!-- Start Text Header -->
+            <div class="col-md-9 col-sm-12">
+                <div class="header-inner">
+                    <div class="header-content">
+                        <h1 class="home-title wow fadeInUp" data-wow-delay="0.2s" data-wow-duration="1s">Приветствуем Вас на официальном сайте базы отдыха <span>Экспедиция</span></h1>
+                        <h4 class="wow fadeInUp" data-wow-delay="0.4s" data-wow-duration="1s">Что может быть лучше отдыха на природе? Только времяпрепровождение на уютной и гостеприимной базе отдыха Экспедиция в Тульской области!</h4>
+                        <div class="bttn-head wow fadeInUp" data-wow-delay="0.6s" data-wow-duration="1s">
+                            <a href="/doma/" class="bttn-vanda-gradient">
+                                <span>Посмотреть дома</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Text Header -->
+            <!-- Start Photo Here -->
+            <div class="col-md-4 col-sm-12 hidden-sm hidden-xs">
 
+            </div>
+            <!-- End Photo Here -->
+        </div>
+    </div>
+    <!-- End Container -->
+    </div>
+    </header>
+    <!-- End Header -->
 
 <?
 require($_SERVER['DOCUMENT_ROOT'].'/bitrix/footer.php');
